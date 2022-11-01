21429fe528e8db6f4f501b6050c4a233ffa62736
Nathan Wai <your@email.address>
2022-11-01 16:51:34 -0400

    Implemented some tests for 400 errors

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -14,14 +14,40 @@ def test_base_route():
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
 
-def test_predict():
+def test_predict1():
     app = Flask(__name__)
     configure_routes(app)
     client = app.test_client()
-    url = '/predict?age=20&absences=3&studytime=300'
+    # Since the studytime is a str when it should be an int should be error
+    url = '/predict?age=20&absences=1&studytime=lots'
 
     response = client.get(url)
 
-    assert response.get_data() == b'to young'
+    assert response.status_code == 400
+
+
+def test_predict2():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    # Since the age is a str when it should be an int should be error
+    url = '/predict?age=twenty&absences=6&studytime=0'
+
+    response = client.get(url)
+
+    assert response.status_code == 400
+
+
+def test_predict3():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    # Since the absences is a str when it should be an int should be error
+    url = '/predict?age=20&absences=six&studytime=0'
+
+    response = client.get(url)
+
+    assert response.status_code == 400
+
 
 
